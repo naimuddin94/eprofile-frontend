71f6a8f8f2dc4b1c1e29f9f80c0ce1954ba31af8
farhan0184 <shahariar0184@gmail.com>
2024-04-04 17:53:43 +0100

    remove error

--- src/app/dashboard/profile/page.jsx
+++ src/app/dashboard/profile/page.jsx
@@ -3,21 +3,21 @@
 import { tabList } from '@/lib/data'
 import React, { Suspense, useEffect, useState } from 'react'
 import { Loading, ProfileTabContents, TabLists } from '../_components'
-import { useSearchParams } from 'next/navigation'
-export const dynamic = 'force-dynamic'
+// import { redirect, useSearchParams } from 'next/navigation'
+// export const dynamic = 'force-dynamic'
 
 
 export default function ProfilePage() {
-
-  let search = useSearchParams()
-  search = search.get('tab') || null
-  const [value, setValue] = useState(search||'name')
+  // let search = useSearchParams()
+  // search = search.get('tab') || null
+  const [value, setValue] = useState('name')
   useEffect(() => {
     window.history.pushState(
       null,
       '',
       `?tab=${value}`
     )
+    
   }, [value])
   return (
     <section className='md:w-[95%]  w-full md:mx-auto py-10 md:flex '>
