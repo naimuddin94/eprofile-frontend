5e1834b941882928fe92067e81d12c2aa8f7e4e5
farhan0184 <shahariar0184@gmail.com>
2024-04-05 15:30:06 +0600

    update

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -2,23 +2,28 @@
 import { useAuthStore } from "@/store/userStore"
 import SideBar from "./_components/share/SideBar"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 const Layout = ({ children }) => {
-  const {isLogin} = useAuthStore()
+  const { isLogin } = useAuthStore()
   const router = useRouter()
-  if(!isLogin){
-    router.push('/')
-    return null
-  }
+  useEffect(() => {
+    if (!isLogin) {
+      if (!isLogin) {
+        router.push('/')
+      }
+    }
+  }, [isLogin])
+
   return (
 
-      <div className='flex ' suppressHydrationWarning={true}>
-        <SideBar />
-        <div className='lg:w-[calc(100%_-_300px)] md:w-[calc(100%_-_220px)] w-full'>
-          {children}
-          
-        </div>
+    <div className='flex ' suppressHydrationWarning={true}>
+      <SideBar />
+      <div className='lg:w-[calc(100%_-_300px)] md:w-[calc(100%_-_220px)] w-full'>
+        {children}
+
       </div>
+    </div>
 
   )
 
