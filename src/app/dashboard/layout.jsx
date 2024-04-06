c7c07513409b875c3fa61f6ca76973597a309d50
farhan0184 <shahariar0184@gmail.com>
2024-04-06 17:20:39 +0600

    commit1

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -4,11 +4,11 @@ import { useAuthStore } from "@/store/userStore"
 // import ProtectedRoute from "@/components/auth/ProtectedRoute"
 import SideBar from "./_components/share/SideBar"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { Suspense, useEffect } from "react"
 
 
 const Layout = ({ children }) => {
-  const {user} = useAuthStore()
+  const { user } = useAuthStore()
   const router = useRouter()
   useEffect(() => {
     if (!user) {
@@ -20,13 +20,15 @@ const Layout = ({ children }) => {
 
   return (
     // <ProtectedRoute>
-        <div className='flex ' suppressHydrationWarning={true}>
-          <SideBar />
-          <div className='lg:w-[calc(100%_-_300px)] md:w-[calc(100%_-_220px)] w-full'>
-            {children}
+    <Suspense fallback={<p>Loading</p>}>
+      <div className='flex ' suppressHydrationWarning={true}>
+        <SideBar />
+        <div className='lg:w-[calc(100%_-_300px)] md:w-[calc(100%_-_220px)] w-full'>
+          {children}
 
-          </div>
         </div>
+      </div>
+    </Suspense>
     //  </ProtectedRoute>
 
   )
